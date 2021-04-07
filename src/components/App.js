670cff305971488d501632fb0ec3cc54c2b70ca6
RICKCOYL <rickoburu@gmail.com>
2021-04-07 12:02:08 +0300

    Pass clickHandler prop to buttonPanel

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,34 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = (buttonName) => {
+    const newState = calculate(this.state, buttonName);
+    this.setState(newState);
+  };
+
+  render() {
+    return (
+      <>
+        <Display />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
 
 export default App;
