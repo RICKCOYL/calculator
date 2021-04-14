ae677c13ba71a8040eff15cef644c004bb60c7ec
RICKCOYL <rickoburu@gmail.com>
2021-04-14 21:11:41 +0300

    finish styling

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -5,37 +5,37 @@ import './style.css';
 
 function ButtonPanel({ clickHandler }) {
   return (
-    <div id="btn-panel" className="">
+    <>
       <div className="button_panel_row">
-        <Button buttonName="AC" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="+/-" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="%" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="÷" clickHandler={clickHandler} buttonClass="button" />
+        <Button buttonName="AC" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="+/-" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="%" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="÷" clickHandler={clickHandler} buttonClass="button button-width" />
       </div>
       <div className="button_panel_row">
-        <Button buttonName="7" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="8" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="9" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="X" clickHandler={clickHandler} buttonClass="button" />
+        <Button buttonName="7" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="8" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="9" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="X" clickHandler={clickHandler} buttonClass="button button-width" />
       </div>
       <div className="button_panel_row">
-        <Button buttonName="4" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="5" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="6" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="-" clickHandler={clickHandler} buttonClass="button" />
+        <Button buttonName="4" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="5" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="6" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="-" clickHandler={clickHandler} buttonClass="button button-width" />
       </div>
       <div className="button_panel_row">
-        <Button buttonName="1" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="2" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="3" clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="+" clickHandler={clickHandler} buttonClass="button" />
+        <Button buttonName="1" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="2" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="3" clickHandler={clickHandler} buttonClass="button_gray button-width" />
+        <Button buttonName="+" clickHandler={clickHandler} buttonClass="button button-width" />
       </div>
       <div className="button_panel_row">
         <Button buttonName="0" clickHandler={clickHandler} buttonClass="zero button_gray" />
-        <Button buttonName="." clickHandler={clickHandler} buttonClass="button_gray" />
-        <Button buttonName="=" clickHandler={clickHandler} buttonClass="button" />
+        <Button buttonName="." clickHandler={clickHandler} buttonClass="button_gray equal" />
+        <Button buttonName="=" clickHandler={clickHandler} buttonClass="button equal" />
       </div>
-    </div>
+    </>
   );
 }
 
